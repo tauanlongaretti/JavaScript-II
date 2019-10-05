f750300efbb7e23d529b54b27b14099b084588c4
tauan-longaretti <tauanlongaretti@gmail.com>
2019-10-04 22:30:23 -0400

    finished closure portion and most of callbacks

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,7 +3,17 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
+const result = () => {
+  let number = 0;
+  return function() {
+    number = number + 3;
+    return number;
+  }
+};
 
+const newResult = result();
+console.log(newResult());
+console.log(newResult());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
